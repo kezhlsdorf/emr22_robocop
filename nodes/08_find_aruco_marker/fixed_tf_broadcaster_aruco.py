2c40d19c80dcf35d88404ad441e8d1fad692f921
Prof. Just <olaf.just@w-hs.de>
2023-06-13 13:31:48 +0200

    Add files via upload

--- nodes/08_find_aruco_marker/fixed_tf_broadcaster_aruco.py
+++ nodes/08_find_aruco_marker/fixed_tf_broadcaster_aruco.py
@@ -14,7 +14,7 @@ import numpy as np  # Scientific computing library for Python
 if __name__ == '__main__':
     rospy.init_node('fixed_tf_broadcaster')
     br = tf.TransformBroadcaster()
-    rate = rospy.Rate(10.0)
+    rate = rospy.Rate(1.0)
     while not rospy.is_shutdown():
         # Hier Abstand camera_link zum world_frame eintragen
         # vorher messen in Meter
@@ -33,12 +33,12 @@ if __name__ == '__main__':
         # new transform
         # (tf.transformations.quaternion_from_euler(-np.pi + 15.0*np.pi/180, np.pi * (0.575), 13.8*np.pi/180, 'sxyz')),  # rpy # (1.0, -0.90, 0.0) (-3.14 , 1.3, 0.0)
                          
-        br.sendTransform((0.0, 0.0, 0.0),
-                         (tf.transformations.quaternion_from_euler(0.0 , 0.0, 0.0)),
+        br.sendTransform((0.45, 0.45, 0.0),
+                         (tf.transformations.quaternion_from_euler(0.0, 0.0, 2.25)),  # rpy
                          rospy.Time.now(),
-                         "camera_link",
-                         "usb_cam")   
-        print("sending transform /usb_cam => /camera_link")
+                         "world",
+                         "marker_id5")  # Reference Frame for transformation  
+        print("sending transform /marker_id5 => /world")
         print(rospy.Time.now())
         rate.sleep()
 
